e22644acfdadf6a905bc6e41e18baed062e72299
Abel Perez Martinez <abel@ABEL-DESKTOP>
2018-08-01 00:57:49 +0100

    Added lesson-04 files

---
+++ lesson-04/serverless-project/src/project.lambda/Function.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Text;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace project.lambda
+{
+    public class LambdaInput
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class LambdaOutput
+    {
+        public string Name { get; set; }
+        public bool Old { get; set; }
+    }
+
+    public class Function
+    {
+        public LambdaOutput HelloHandler(LambdaInput input, ILambdaContext context)
+        {
+            context.Logger.Log($"Hello {input.Name}, you are now {input.Age}");
+            return new LambdaOutput { Name = input.Name, Old = input.Age > 50 };
+        }
+    }
+}
